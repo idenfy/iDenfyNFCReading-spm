6deaaa0cf7b81e565857a0752747aacc0755d723
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-09-26 04:30:31 +0000

    new version

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 
 import PackageDescription
 
-let version = "8.5.3"
+let version = "8.5.7"
 
 enum Checksums {
-    static let iDenfyInternalLoggerChecksum = "0580ee1428687aa0890afbc8f99ea55491b11cb0f76b1068e8cf85929074445c"
-    static let FaceTecSDKChecksum = "bfc95ea65efef986fb8009cb2039ff93e78408dd9fbc49765d299acf5ddb574c"
-    static let iDenfyLivenessChecksum = "5b24dfee0b17eda9a149c566115172c29e815196932d720b283e511aeeb82185"
-    static let idenfyviewsChecksum = "904975ee7b40e52c154de6d05e4f93ac6e01ac09f7fd530b35994e64081a605d"
-    static let iDenfySDKChecksum = "1265bc36460a58c3c022738a6126fa2be6febf966e9f65f6f23d2b9261fd9a39"
-    static let idenfycoreChecksum = "711dcab4dd7953a13ba6411fb35668f90fbb8455706ebfe2b5e06ad8f289e690"
-    static let idenfyNFCReadingChecksum = "053c47a43c298082ca7f60d3c878085f2b74df43f25f490ec226edbbcf8acb1b"
-    static let openSSLChecksum = "367aec4020eab1dff8714160b5f231c5e5632223a1bc5d147f57ec3e385f1209"
+    static let iDenfyInternalLoggerChecksum = "f64493378edf75639bf7eb17c74f1e9993ad7888d9c1e134bbf5f4789598027d"
+    static let FaceTecSDKChecksum = "4c2ae61c42173d722b037576f2c91ce18112b0e62321750b0c7ac9c2e2df2118"
+    static let iDenfyLivenessChecksum = "248212bc00843e617598ae89b6d94e1f4b06946fbb364993557ff7e3d165d12d"
+    static let idenfyviewsChecksum = "3f791932898428cb34c1e28cdf0458aa7d8e99d37307550e819745775a17148a"
+    static let iDenfySDKChecksum = "a7b14a9a8016a16cb3ab284add5afb7b2e1c3d861c29699011827246712580cb"
+    static let idenfycoreChecksum = "ad5a098213f3e167927e215a153da95e7e6e198f460802aa69efe734730d1b27"
+    static let idenfyNFCReadingChecksum = "92ba9a98de8a2abb3143093748a83ccaed2837528ac2283cd5a5c7e60ad671d4"
+    static let openSSLChecksum = "a9c26d1315432cda3fbf63e19856db9f987f58cb8f94cc28d6dd660826d90747"
 }
 
 let package = Package(
